a7145caa08fea0036118e7d9d3750e1fffac926b
Caimin Ayres <110409985+caimin9@users.noreply.github.com>
2023-11-25 11:40:52 +0000

    Update LV.cs

--- EULER/LV.cs
+++ EULER/LV.cs
@@ -9,14 +9,34 @@ namespace EulerVector
 			get { return beta; }
 			set { beta = value; }
 		}
-		public double alpha = 1;
-		public double delta = 0.1;
-		public double gamma = 1;
+		
+		private double alpha = 1;
+		public double Alpha
+		{
+			get { return alpha; }
+			set { alpha = value; }
+		}
+		
+		private double delta = 0.1;
+		public double Delta
+		{
+			get{return delta; }
+			set{delta = value }
+		}
+		
+		private double gamma = 1;
+		public double Gamma
+		{
+			get{return gamma}
+			set{gamma = value}
+		}
+			
 		public LV()
 		{
 		}
 		public Vector LVFunc(Vector v, double t)
 		{//x is v[0] and y is v[1]
+			//This is our lotka voltera formula
 			Vector tmp = new Vector(v.Size);
 			tmp[0] = alpha * v[0] - beta * v[0] * v[1];
 			tmp[1]  = delta * v[0]*v[1] -gamma *v[1];
